a1ee8f92bacde4b13e20d7f487b12fa088953675
JuanSebastianBorja <borja.juan@correounivalle.edu.co>
2025-12-07 18:16:05 -0500

    config para render

--- web/js/config.js
+++ web/js/config.js
@@ -5,5 +5,12 @@ const isLocal = window.location.hostname === 'localhost';
 export const CONFIG = {
   API_BASE_URL: isLocal
     ? 'http://localhost:3000'
-    : 'https://quetzal-backend.onrender.com'
+    : 'https://quetzal-backend.onrender.com',
+
+    STORAGE_KEYS: {
+    TOKEN: 'quetzal_token',
+    USER_ROLE: 'quetzal_user_role',
+    USER_DATA: 'quetzal_user_data'
+  }
 };
+
